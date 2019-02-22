7447a03851aa544659932f3a0c6286890a36624b
Retorillo <retorillo@outlook.com>
2019-02-22 11:56:25 +0900

    Supports clipping masked layers

--- live2d-mini-preprocess.jsx
+++ live2d-mini-preprocess.jsx
@@ -1,34 +1,60 @@
 var doc = app.activeDocument;
 doc.suspendHistory('Live2D Mini Preprocess', 'exec()');
 
+function safeName(name) {
+  // NOTE: Cubism may fail to load if layer has contains dot
+  return name.replace(/\./g, '-');
+}
+
 function seq(a) {
   var r = [];
   for (var c = 0; c < a.length; c++)
     r.push(a[c]);
   r.each = function(m) {
-    for (var c = 0; c < this.length; c++) m(this[c]); 
+    for (var c = 0; c < this.length; c++)
+      m(this[c]); 
   };
+  r.reversed_each = function(m) {
+    for (var c = this.length - 1; c >= 0; c--)
+      m(this[c]);
+  }
   return r;
 } 
 
 function handleArtLayers(layers) {
+  // NOTE: grouped = clipping masked
+  groupedLayers = seq([]);
   seq(layers).each(function(layer) {
+    layer.name = safeName(layer.name);
     if (!layer.visible) {
       layer.remove();
       return;
     }
     if (layer.isBackgroundLayer)
       return;
+    if (layer.grouped) {
+      groupedLayers.push(layer);
+      return
+    }
     set = doc.layerSets.add();
     set.name = layer.name;
     set.move(layer, ElementPlacement.PLACEBEFORE);
     layer.move(set, ElementPlacement.INSIDE);
+
+    placeTarget = layer;
+    groupedLayers.reversed_each(function(l) {
+      l.move(placeTarget, ElementPlacement.PLACEBEFORE);
+      l.grouped = true;
+      placeTarget = l;
+    });
+    groupedLayers = seq([]);
     set.merge(); 
   });
 }
 
 function handleLayerSets(sets) {
   seq(sets).each(function(set) {
+    set.name = safeName(set.name);
     if (!set.visible || !(set.artLayers.length + set.layerSets.length))  {
       set.remove();
       return;
