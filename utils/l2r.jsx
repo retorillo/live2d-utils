3eba15bd2e17c8ac3471d8f611e1b9ff66082889
Retorillo <retorillo@outlook.com>
2019-02-26 06:45:04 +0900

    Fix bug about layer-set with clipping mask, and invisible layers on l2r

--- utils/l2r.jsx
+++ utils/l2r.jsx
@@ -1,10 +1,12 @@
-// Sync left content to right recursively
-// WARNING: This script does not support Vector and Layer masks.
+// sync right content with left content while keeping layer structure
+// WARNING: This script does not support vector and layer mask.
+// NOTE: If name of layer or layer set starting with #, its operation will be skipped.
+
 var doc = app.activeDocument;
 doc.suspendHistory('Sync left content to right', 'exec()');
 
 function map(list, mapper) {
-  mapped = [];
+  var mapped = [];
   for (var c = 0; c < list.length; c++)
     mapped.push(mapper(list[c]));
   return mapped;
@@ -12,13 +14,13 @@ function map(list, mapper) {
 function unitToNr(val) {
   return parseFloat(/^[.0-9]+/.exec(val));
 }
-function resetLayerState(layer) {
-  props = [ ['blendMode', BlendMode.NORMAL],
+function resetArtLayer(l) {
+  var props = [ ['blendMode', BlendMode.NORMAL],
             ['fillOpacity', 100], 
             ['pixelsLocked', false],
             ['transparentPixelsLocked', false],
             ['visible', true] ];
-  state = {};
+  var state = {};
   state.applyTo = function(l) {
     s = this;
     map(props, function(p) {
@@ -27,59 +29,69 @@ function resetLayerState(layer) {
     });
   };
   map(props, function(p) {
-    state[p[0]] = layer[p[0]];
-    layer[p[0]] = p[1];
+    state[p[0]] = l[p[0]];
+    l[p[0]] = p[1];
   });
   return state;
 }
-function l2r(layer) {
-  state = resetLayerState(layer);
-  c = unitToNr(doc.width) / 2;
-  h = doc.height;
-  w = unitToNr(doc.width);
-  leftRegion = [[0, 0], [c, 0], [c, h], [0, h], [0, 0]];
-  rightRegion = [[c, 0], [w, 0], [w, h], [c, h], [c, 0]];
-  doc.activeLayer = layer;
+function boundsToRect(lb) {
+  // NOTE: bounds is LTRB format: [x1, y1, x2, y2]
+  var b = map(lb, unitToNr);
+  return { x: b[0], y: b[1], w: b[2] - b[0], h: b[3] - b[1],
+    empty: function() { return this.w == this.h && this.h == 0; } }; 
+}
+function l2r(l) {
+  var state = resetArtLayer(l);
+  var c = unitToNr(doc.width) / 2;
+  var h = doc.height;
+  var w = unitToNr(doc.width);
+  var leftRegion = [[0, 0], [c, 0], [c, h], [0, h], [0, 0]];
+  var rightRegion = [[c, 0], [w, 0], [w, h], [c, h], [c, 0]];
+  doc.activeLayer = l;
   doc.selection.select(rightRegion);
   doc.selection.clear();
   doc.selection.deselect();
-  right = layer.duplicate(layer, ElementPlacement.PLACEBEFORE);
+  var lb = boundsToRect(l.bounds);
+  if (lb.empty())
+    return l;
+  var right = l.duplicate(l, ElementPlacement.PLACEBEFORE);
   right.resize(-100, 100, AnchorPosition.MIDDLECENTER);
-  bounds = map(layer.bounds, unitToNr);
-  // NOTE: bounds is LTRB format: [x1, y1, x2, y2]
-  x = bounds[0]
-  lw = bounds[2] - bounds[0];
-  expected_x = w - (x + lw);
-  right.translate(expected_x - x, 0);
+  expect = w - (lb.x + lb.w);
+  right.translate(expect - lb.x, 0);
   merged = right.merge(); 
   state.applyTo(merged);
   return merged;
 }
-function handleArtLayers(layers) {
-  groups = []
+function handleLayers(layers) {
   map(layers, function(l) {
-    if (l.grouped) {
-      l.grouped = false;
-      groups.splice(0, 0, l);
-      return;
+    if (!this.groups)
+      this.groups = []
+    if (/^#/.test(l.name)) return;
+    switch (l.typename) {
+      case 'ArtLayer':
+        if (l.grouped) {
+          l.grouped = false;
+          this.groups.splice(0, 0, l);
+          return;
+        }
+        l2r(l);
+        break;
+      case 'LayerSet':
+        var visible = l.visible;
+        l.visible = true;
+        handleLayers(l.layers);
+        l.visible = visible; 
+        break;
+      default:
+        return;
     }
-    placeTarget = l2r(l);
-    map(groups, function(g) {
+    map(this.groups, function(g) {
       l2r(g);
       g.grouped = true;
     });
-    groups = [];
-  });
-}
-function handleLayerSets(sets) {
-  map(sets, function(s) {
-    handleArtLayers(s.artLayers);
-    handleLayerSets(s.layerSets);
+    this.groups = [];
   });
 }
 function exec() {
-  if (doc.activeLayer.typename == 'ArtLayer')
-    handleArtLayers([doc.activeLayer]);
-  else
-    handleLayerSets([doc.activeLayer]);
+  handleLayers([doc.activeLayer]);
 }
